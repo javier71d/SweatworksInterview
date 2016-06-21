7db1c8cb73d469549d8010f2644b85ec4c7991c4
javier71d <javier71d@gmail.com>
2016-06-21 17:37:45 -0300

    Code to generate numbers

--- controller.js
+++ controller.js
@@ -1,4 +1,30 @@
 angular.module('test', [])
 .controller('testController', ['$scope', function($scope) {
-	
+	$scope.showNumbers = function(type){
+
+		$scope.errors = false;
+
+		if($scope.max > $scope.min){
+
+			$scope.numbers = {};
+
+			for (var i = $scope.min; i <= $scope.max; i++) {
+
+				if(i % 2 == 0){
+					$scope.numbers[i] = 'even';				
+				} else {
+					$scope.numbers[i] = 'odd';
+				}
+
+			}
+
+		} else {
+
+			$scope.errors = "Range error";
+
+		}
+
+		$scope.type = type;
+
+	};
 }]);
